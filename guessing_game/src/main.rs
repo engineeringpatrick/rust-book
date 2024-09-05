6893b184149207b7c7836765a0cceed0aaf63bcb
Patrick Deniso <patrickdeniso@gmail.com>
2024-09-05 16:42:22 -0400

    finish guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!");
+
+    let secret = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        print!("Input your guess: ");
+        std::io::stdout().flush().unwrap(); // unwrap() extracts the result and panics if error
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+    }
 }
